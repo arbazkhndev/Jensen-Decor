68fc404469d528e33c67b1e196dd6af9473eee28
Arbaz Khan <arbaz.khan.7.21.11@gmail.com>
2025-03-18 02:58:02 +0500

    add compare

--- Assets/JavaScript/campare.js
+++ Assets/JavaScript/campare.js
@@ -15,48 +15,48 @@ function selectProduct1()
     }
     else if(product=="val2")
     {
-        document.getElementById("image").src="Assets/Item-Image/img1.webp";
-        document.getElementById("model").innerHTML="Name : GOBIUS FISH ART";
-        document.getElementById("features").innerHTML="Description: Gobius is a genus of fish in the family Gobiidae native to fresh, brackish and marine waters.<br>";
-        document.getElementById("spec").innerHTML="Specifications:  Modern & Industrial Style | Matte Black Paint | You can hang the product in any directions.<br>";;
+        document.getElementById("image").src="Assets/Item-Image/img2.webp";
+        document.getElementById("model").innerHTML="Name : LOVE BIRDS WALL ART";
+        document.getElementById("features").innerHTML="Description: To make a little difference and bring a lovely atmosphere to your home. Your guests will fall in love with its unique design and they can't take their eyes off it.<br>";
+        document.getElementById("spec").innerHTML="Specifications:  Modern & Industrial Style | Matte Black Paint | You can hang the product in any directions | Easy to hang Size.<br>";;
         
     }
     else if(product=="val3")
     {
-        document.getElementById("image").src="Assets/Item-Image/img1.webp";
-        document.getElementById("model").innerHTML="Name : GOBIUS FISH ART";
-        document.getElementById("features").innerHTML="Description: Gobius is a genus of fish in the family Gobiidae native to fresh, brackish and marine waters.<br>";
-        document.getElementById("spec").innerHTML="Specifications:  Modern & Industrial Style | Matte Black Paint | You can hang the product in any directions.<br>";
+        document.getElementById("image").src="Assets/Item-Image/img3.webp";
+        document.getElementById("model").innerHTML="Name : SUPER HERO WALL ART";
+        document.getElementById("features").innerHTML="Description: Made of high quality iron, elegant, fashionable & durable, ensure long-lasting functionality and beauty. Avengers Face Portraits Wood Wall Art Set, Geometric Framed Avenger Superheroes Wall Decor, Wooden Spiderman Thor Hulk Ironman Wall Hanging.<br>";
+        document.getElementById("spec").innerHTML="Specifications:  Modern & Industrial Style | Matte Black Paint | You can hang the product in any directions | Easy to hang.<br>";
         
     }
     else if(product=="val4")
     {
-        document.getElementById("image").src="Assets/Item-Image/img1.webp";
-        document.getElementById("model").innerHTML="Name : GOBIUS FISH ART";
-        document.getElementById("features").innerHTML="Description: Gobius is a genus of fish in the family Gobiidae native to fresh, brackish and marine waters.<br>";
-        document.getElementById("spec").innerHTML="Specifications:  Modern & Industrial Style | Matte Black Paint | You can hang the product in any directions.<br>";
+        document.getElementById("image").src="Assets/Item-Image/peelartimg1.webp";
+        document.getElementById("model").innerHTML="Name : Abstract M08 Wallpaper";
+        document.getElementById("features").innerHTML="Description: Add an industrial flare to your favorite designs. Our metal prints feature super sharp image details and rich, vibrant colors on a lightweight yet durable aluminum canvas. High gloss finish enhances color and produces sharp image details.<br>";
+        document.getElementById("spec").innerHTML="Specifications:  Collapse | Printed on 1/16 thick aluminum sheet canvas | High-gloss finish enhances image and colors | 3/4 wooden frame on back to offset from wall | Wire or sawtooth hanger depending on size (hanging hardware not included)<br>";
         
     }
     else if(product=="val5")
     {
         document.getElementById("image").src="Assets/Item-Image/img1.webp";
-        document.getElementById("model").innerHTML="Name : GOBIUS FISH ART";
-        document.getElementById("features").innerHTML="Description: Gobius is a genus of fish in the family Gobiidae native to fresh, brackish and marine waters.<br>";
-        document.getElementById("spec").innerHTML="Specifications:  Modern & Industrial Style | Matte Black Paint | You can hang the product in any directions.<br>";
+        document.getElementById("model").innerHTML="Name : LEAVES PATTERN PINK LOVE Wallpaper";
+        document.getElementById("features").innerHTML="Description: Add an industrial flare to your favorite designs. Our metal prints feature super sharp image details and rich, vibrant colors on a lightweight yet durable aluminum canvas.<br>";
+        document.getElementById("spec").innerHTML="Specifications:  Collapse | Printed on 1/16 thick aluminum sheet canvas | High-gloss finish enhances image and colors | 3/4 wooden frame on back to offset from wall | Wire or sawtooth hanger depending on size (hanging hardware not included)<br>";
         
     }
      else if(product=="val6")
     {
         document.getElementById("image").src="Assets/Item-Image/img1.webp";
-        document.getElementById("model").innerHTML="Name : GOBIUS FISH ART";
-        document.getElementById("features").innerHTML="Description: Gobius is a genus of fish in the family Gobiidae native to fresh, brackish and marine waters.<br>";
-        document.getElementById("spec").innerHTML="Specifications:  Modern & Industrial Style | Matte Black Paint | You can hang the product in any directions.<br>";
+        document.getElementById("model").innerHTML="Name : Vibrant color block pattern Wallpaper";
+        document.getElementById("features").innerHTML="Description: Add an industrial flare to your favorite designs. Our metal prints feature super sharp image details and rich, vibrant colors on a lightweight yet durable aluminum canvas.<br>";
+        document.getElementById("spec").innerHTML="Specifications:  Collapse | Printed on 1/16 thick aluminum sheet canvas | High-gloss finish enhances image and colors | 3/4 wooden frame on back to offset from wall | Wire or sawtooth hanger depending on size (hanging hardware not included)<br>";
         
     }
      else if(product=="val7")
     {
         document.getElementById("image").src="Assets/Item-Image/img1.webp";
-        document.getElementById("model").innerHTML="Name : GOBIUS FISH ART";
+        document.getElementById("model").innerHTML="Name : Hendley 18 Opening Wall Gallery";
         document.getElementById("features").innerHTML="Description: Gobius is a genus of fish in the family Gobiidae native to fresh, brackish and marine waters.<br>";
         document.getElementById("spec").innerHTML="Specifications:  Modern & Industrial Style | Matte Black Paint | You can hang the product in any directions.<br>";
         
@@ -64,7 +64,7 @@ function selectProduct1()
      else if(product=="val8")
     {
         document.getElementById("image").src="Assets/Item-Image/img1.webp";
-        document.getElementById("model").innerHTML="Name : GOBIUS FISH ART";
+        document.getElementById("model").innerHTML="Name : Bryleigh 12 Slot Wood Collage";
         document.getElementById("features").innerHTML="Description: Gobius is a genus of fish in the family Gobiidae native to fresh, brackish and marine waters.<br>";
         document.getElementById("spec").innerHTML="Specifications:  Modern & Industrial Style | Matte Black Paint | You can hang the product in any directions.<br>";
         
@@ -72,63 +72,63 @@ function selectProduct1()
      else if(product=="val9")
     {
         document.getElementById("image").src="Assets/Item-Image/img1.webp";
-        document.getElementById("model").innerHTML="Name : GOBIUS FISH ART";
+        document.getElementById("model").innerHTML="Name : Westbury Black MDF Picture Frame";
         document.getElementById("features").innerHTML="Description: Gobius is a genus of fish in the family Gobiidae native to fresh, brackish and marine waters.<br>";
         document.getElementById("spec").innerHTML="Specifications:  Modern & Industrial Style | Matte Black Paint | You can hang the product in any directions.<br>";
         
     }
     else if(product=="val10")
         {
-            document.getElementById("image").src="Assets/Item-Image/img1.webp";
-            document.getElementById("model").innerHTML="Name : GOBIUS FISH ART";
-            document.getElementById("features").innerHTML="Description: Gobius is a genus of fish in the family Gobiidae native to fresh, brackish and marine waters.<br>";
-            document.getElementById("spec").innerHTML="Specifications:  Modern & Industrial Style | Matte Black Paint | You can hang the product in any directions.<br>";;
+            document.getElementById("image").src="Assets/Item-Image/mirror/beemain.webp";
+            document.getElementById("model").innerHTML="Name : Genean Metal Abstract Mirror";
+            document.getElementById("features").innerHTML="Description: This accent mirror consists of multi-wire circles. It stands out with its intricate details and understated lines. The matte gold foil with a slightly and randomly brushed antique finish gives it a luxurious look.<br>";
+            document.getElementById("spec").innerHTML="Specifications:  Sizes and material: (34 x 17.7) Iron framed | Matte gold Paint | You can hang the product in any directions | Easy to clean: Please use soft, dry clothes to clean this mirror | Suitable for indoor and outdoor use.<br>";;
             
         }
         else if(product=="val11")
         {
-            document.getElementById("image").src="Assets/Item-Image/img1.webp";
-            document.getElementById("model").innerHTML="Name : GOBIUS FISH ART";
-            document.getElementById("features").innerHTML="Description: Gobius is a genus of fish in the family Gobiidae native to fresh, brackish and marine waters.<br>";
-            document.getElementById("spec").innerHTML="Specifications:  Modern & Industrial Style | Matte Black Paint | You can hang the product in any directions.<br>";
+            document.getElementById("image").src="Assets/Item-Image/mirror/feather_wall_mirror_.jpg";
+            document.getElementById("model").innerHTML="Name : Golden Feathers Mirror";
+            document.getElementById("features").innerHTML="Description: Bring elegance and sophistication to your space with the Wall Decorative Mirror – Golden Feathers. Designed to captivate, this stunning mirror features an intricate feather-inspired frame with a luxurious gold finish, adding warmth and charm to any room.<br>";
+            document.getElementById("spec").innerHTML="Specifications:  Sizes and material: (34 x 17.7) Iron framed | Matte gold Paint | You can hang the product in any directions | Easy to clean: Please use soft, dry clothes to clean this mirror | Suitable for indoor and outdoor use.<br>";;
             
         }
         else if(product=="val12")
         {
-            document.getElementById("image").src="Assets/Item-Image/img1.webp";
-            document.getElementById("model").innerHTML="Name : GOBIUS FISH ART";
-            document.getElementById("features").innerHTML="Description: Gobius is a genus of fish in the family Gobiidae native to fresh, brackish and marine waters.<br>";
-            document.getElementById("spec").innerHTML="Specifications:  Modern & Industrial Style | Matte Black Paint | You can hang the product in any directions.<br>";
+            document.getElementById("image").src="Assets/Item-Image/mirror/Flower_Wall_Mirror_.jpg";
+            document.getElementById("model").innerHTML="Name : Metallic Flower Mirror";
+            document.getElementById("features").innerHTML="Description: Enhance your space with the elegance of our Wall Decorative Mirror - Metallic Flower. This exquisite piece combines functionality with artistic beauty.<br>";
+            document.getElementById("spec").innerHTML="Specifications:  Sizes and material: (34 x 17.7) Iron framed | Matte gold Paint | Easy to clean: Please use soft, dry clothes to clean this mirror | Suitable for indoor and outdoor use.<br>";;
             
         }
         else if(product=="val13")
         {
-            document.getElementById("image").src="Assets/Item-Image/img1.webp";
-            document.getElementById("model").innerHTML="Name : GOBIUS FISH ART";
-            document.getElementById("features").innerHTML="Description: Gobius is a genus of fish in the family Gobiidae native to fresh, brackish and marine waters.<br>";
-            document.getElementById("spec").innerHTML="Specifications:  Modern & Industrial Style | Matte Black Paint | You can hang the product in any directions.<br>";
+            document.getElementById("image").src="Assets/Item-Image/mirror/shelve1.webp";
+            document.getElementById("model").innerHTML="Name : Modern Glam Floating Wall Shelf";
+            document.getElementById("features").innerHTML="Description: Create a usable wall display in your bathroom, kitchen, dining room or in any room of the house that needs great looking open storage or display.<br>";
+            document.getElementById("spec").innerHTML="Specifications:  Small 9 inch circle 6 inch depth Large 13 inch circle 6 inch depth | Mirror Material: Glass | High-Quality Laminated MDF Build | Beveled clear mirror squares | High-Quality Metal Build Powder Coated.<br>";
             
         }
          else if(product=="val14")
         {
-            document.getElementById("image").src="Assets/Item-Image/img1.webp";
-            document.getElementById("model").innerHTML="Name : GOBIUS FISH ART";
-            document.getElementById("features").innerHTML="Description: Gobius is a genus of fish in the family Gobiidae native to fresh, brackish and marine waters.<br>";
-            document.getElementById("spec").innerHTML="Specifications:  Modern & Industrial Style | Matte Black Paint | You can hang the product in any directions.<br>";
+            document.getElementById("image").src="Assets/Item-Image/mirror/shelve2.webp";
+            document.getElementById("model").innerHTML="Name : Wall Display Shelf";
+            document.getElementById("features").innerHTML="Description: We love this 3-tiered floating shelf and how it brings rustic charm and handy storage to your bare farmhouse walls. It's made with an open black metal frame and has three solid pinewood shelves with a distressed finish for a mixed-material look.<br>";
+            document.getElementById("spec").innerHTML="Specifications:  Decorative floating wall shelves with black metal frames and 3 wood shelves | Floating shelves can be used for home organization and display in any room of the home | Overall Shape: Rectangle.<br>";
             
         }
          else if(product=="val15")
         {
-            document.getElementById("image").src="Assets/Item-Image/img1.webp";
-            document.getElementById("model").innerHTML="Name : GOBIUS FISH ART";
-            document.getElementById("features").innerHTML="Description: Gobius is a genus of fish in the family Gobiidae native to fresh, brackish and marine waters.<br>";
-            document.getElementById("spec").innerHTML="Specifications:  Modern & Industrial Style | Matte Black Paint | You can hang the product in any directions.<br>";
+            document.getElementById("image").src="Assets/Item-Image/mirror/shelve3.webp";
+            document.getElementById("model").innerHTML="Name : Phantom 4 Piece Floating Shelf";
+            document.getElementById("features").innerHTML="Description: Add an industrial flare to your favorite designs. Our metal prints feature super sharp image details and rich, vibrant colors on a lightweight yet durable aluminum canvas.<br>";
+            document.getElementById("spec").innerHTML="Specifications:  Wood features natural grains and variations. | Keyhole slot hanging mechanism. | Easy and quick assembly | Strong and sturdy construction. | Ample open display storage space.<br>";
             
         }
          else if(product=="val16")
         {
             document.getElementById("image").src="Assets/Item-Image/img1.webp";
-            document.getElementById("model").innerHTML="Name : GOBIUS FISH ART";
+            document.getElementById("model").innerHTML="Name : Metal Horse Wall Decor";
             document.getElementById("features").innerHTML="Description: Gobius is a genus of fish in the family Gobiidae native to fresh, brackish and marine waters.<br>";
             document.getElementById("spec").innerHTML="Specifications:  Modern & Industrial Style | Matte Black Paint | You can hang the product in any directions.<br>";
             
@@ -136,7 +136,7 @@ function selectProduct1()
          else if(product=="val17")
         {
             document.getElementById("image").src="Assets/Item-Image/img1.webp";
-            document.getElementById("model").innerHTML="Name : GOBIUS FISH ART";
+            document.getElementById("model").innerHTML="Name : Deer Handcraft Iron";
             document.getElementById("features").innerHTML="Description: Gobius is a genus of fish in the family Gobiidae native to fresh, brackish and marine waters.<br>";
             document.getElementById("spec").innerHTML="Specifications:  Modern & Industrial Style | Matte Black Paint | You can hang the product in any directions.<br>";
             
@@ -144,7 +144,7 @@ function selectProduct1()
         else if(product=="val18")
         {
             document.getElementById("image").src="Assets/Item-Image/img1.webp";
-            document.getElementById("model").innerHTML="Name : GOBIUS FISH ART";
+            document.getElementById("model").innerHTML="Name : Wall Decor on Resin";
             document.getElementById("features").innerHTML="Description: Gobius is a genus of fish in the family Gobiidae native to fresh, brackish and marine waters.<br>";
             document.getElementById("spec").innerHTML="Specifications:  Modern & Industrial Style | Matte Black Paint | You can hang the product in any directions.<br>";
             
